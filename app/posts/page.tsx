8ae9e7c56015e088a9e9f2296aec0905caf69f99
LoicDumat <137074179+LoicDumat@users.noreply.github.com>
2024-08-14 17:07:01 +0200

    fix params

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -19,7 +19,7 @@ export default async function BlogPage() {
       {posts.map(({ id, title }) => (
         <article key={title}>
           <h2>
-            <Link  href={`/posts/${encodeURIComponent(title)}`} className="text-lg font-bold">
+            <Link href={`/posts/${id}`} className="text-lg font-bold">
               {title}
             </Link>
           </h2>
