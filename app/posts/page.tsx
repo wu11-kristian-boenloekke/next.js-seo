b994685ac8cfb38945130137bb9a037108e65619
LoicDumat <137074179+LoicDumat@users.noreply.github.com>
2024-08-14 21:30:02 +0200

    add URL title slug

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,30 +1,36 @@
 import { BlogPostsResponse } from "@/model/BlogPost";
 import { Metadata } from "next";
 import Link from "next/link";
+import { createSlug } from "@/utils/createSlug";
 
 export const metadata: Metadata = {
     title: 'Blog',
     description: 'This is a description of blog-posts',
 }
 
+
+
 export default async function BlogPage() {
   const response = await fetch("https://dummyjson.com/posts");
   const { posts }: BlogPostsResponse = await response.json();
 
-//   await delay(1000);
+  
 
   return (
     <div className="max-w-prose m-auto space-y-5">
       <h1 className="text-3xl text-center mb-3 font-bold">Posts</h1>
-      {posts.map(({ id, title }) => (
-        <article key={title}>
-          <h2>
-            <Link href={`/posts/${id}`} className="text-lg font-bold">
-              {title}
-            </Link>
-          </h2>
-        </article>
-      ))}
+      {posts.map(({ id, title }) => {
+        const slug = createSlug(title);
+        return (
+          <article key={id}>
+            <h2>
+              <Link href={`/posts/${id}-${slug}`} className="text-lg font-bold">
+                {title}
+              </Link>
+            </h2>
+          </article>
+        );
+      })}
     </div>
   );
 }
---
+++ utils/createSlug.ts
@@ -0,0 +1,6 @@
+export function createSlug(title: string): string {
+    return title
+        .toLowerCase()
+        .replace(/[^a-z0-9]+/g, '-')
+        .replace(/^-+|-+$/g, '');
+  }
